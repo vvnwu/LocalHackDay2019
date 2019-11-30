113c450d6b85214e5d24b2d24ea8fa57110f4d81
wuvvn <wu.vivian00@gmail.com>
2019-11-30 14:55:53 -0800

    added firebase init, and began entity schema

---
+++ firebase/prof.js
@@ -0,0 +1,12 @@
+class prof  {
+    constructor(room) {
+        this.room = room;
+        this.markQuestion= function(questionID){
+            /* 'post' req to db; set question.answered to true*/
+        }
+        this.deleteRoom= function(){
+            /*delete this' room from db*/
+        }
+
+    }
+}
