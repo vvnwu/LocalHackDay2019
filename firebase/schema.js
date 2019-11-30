ef3a8caec4166050139307ada797e0fb363c129e
Kevin <kevin.k.f.lam1@gmail.com>
2019-11-30 15:16:19 -0800

    Added schema

--- firebase/schema.js
+++ firebase/schema.js
@@ -7,7 +7,29 @@ class prof  {
         this.deleteRoom= function(){
             /*delete this' room from db*/
         }
+    }
+}
 
+class student  {
+    constructor(room) {
+        this.room = room;
+        this.upVoteQuestion= function(questionID){
+            /* 'post' req to db; set question answered to true*/
+            questionID.likes++;
+        }
+        this.postQuestion= function(question){
+            /*posts the question to the room*/
+        }
+    }
+}
+
+class question  {
+    constructor(room) {
+        this.id = id;
+        this.likes = 0;
+        this.goodQuestion = false;
+        this.answered = false;
+        this.
     }
 }
 
@@ -20,32 +42,5 @@ class prof  {
         this.deleteRoom= function(){
             /*delete this' room from db*/
         }
-
-    }
-}
-
-class prof  {
-    constructor(room) {
-        this.room = room;
-        this.markQuestion= function(questionID){
-            /* 'post' req to db; set question.answered to true*/
-        }
-        this.deleteRoom= function(){
-            /*delete this' room from db*/
-        }
-
-    }
-}
-
-class prof  {
-    constructor(room) {
-        this.room = room;
-        this.markQuestion= function(questionID){
-            /* 'post' req to db; set question.answered to true*/
-        }
-        this.deleteRoom= function(){
-            /*delete this' room from db*/
-        }
-
     }
 }
