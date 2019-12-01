766dacbdf15a0bbd78137c04e9d4f3910957cfa6
Kevin <kevin.k.f.lam1@gmail.com>
2019-11-30 17:41:55 -0800

    fixed schema

--- firebase/schema.js
+++ firebase/schema.js
@@ -24,18 +24,29 @@ class student  {
     constructor(room) {
         this.room = room;
         this.upVoteQuestion= function(questionID){
-            /* 'post' req to db; set question answered to true*/
-            questionID.likes++;
+                   /* 'post' req to db; set question.answered to true*/
+                   database.ref('rooms/'+room+'/questions/' + questionID).once('value').then(function(snapshot) {
+                    var question = {
+                        id : snapshot.val().id,
+                        likes : snapshot.val().likes+1,
+                        goodQuestion : snapshot.val().goodQuestion,
+                        answered : true
+                    }
+                    console.log(question);
+                    database.ref('rooms/'+room+'/questions/' + questionID).set(question);
+                  });;
         }
         this.postQuestion= function(question){
             /*posts the question to the room*/
+            newQuestion = new question();
         }
     }
 }
 
 class question  {
     constructor(room) {
-        this.id = id;
+        var newPostKey = firebase.database().ref().child('/questions').push().key;
+        this.id = newPostKey;
         this.likes = 0;
         this.goodQuestion = false;
         this.answered = false;
@@ -45,11 +56,5 @@ class question  {
 class room  {
     constructor(room) {
         this.room = room;
-        this.markQuestion= function(questionID){
-            /* 'post' req to db; set question.answered to true*/
-        }
-        this.deleteRoom= function(){
-            /*delete this' room from db*/
-        }
     }
 }
